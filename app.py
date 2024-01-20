0367559fc8b53809f9755499afb59609244599b7
André Luis Quiosi <andre.quiosi@outlook.com>
2024-01-20 09:59:30 -0300

    Trabalhando com variáveis, condicionáis e funções

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ print('1. Cadastrar Restaurante')
 print('2. Listar restaurantes')
 print('3. Ativar Reustaurante')
 print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+# print(f'Você enscolheu a opção {opcao_escolhida}')
+
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando programa')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    finalizar_app()
