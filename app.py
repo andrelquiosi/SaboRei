88f287ad593eda20e114f47b85e1a40ec75a8907
André Luis Quiosi <andre.quiosi@outlook.com>
2024-01-20 14:24:07 -0300

    Exploramos o conceito de condicionais if-else e compreendemos a importância do seu uso para estabelecer o funcionamento do nosso código de acordo com uma condição; Compreendemos a importância de utilizar funções para organizar e melhorar o funcionamento de nosso projeto; Entendemos como podemos aproveitar funções já criadas através do uso do import de um módulo para limpar nosso terminal a fim de melhorar a visualização das informações; Aprendemos o conceito de casting para indicar o tipo de uma variável ao receber uma informação do usuário, melhorando a segurança e eficácia do nosso código.

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 import os
 
-print("""
-████████████████████████████████████████
-█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▄█
-█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄██─▄█▀██─██
-▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀
-""")
+def exibir_nome_do_programa():
+    print("""
+    ████████████████████████████████████████
+    █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▄█
+    █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄██─▄█▀██─██
+    ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀
+    """)
 
-print('1. Cadastrar Restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar Reustaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-# print(f'Você enscolheu a opção {opcao_escolhida}')
+def exibir_opcoes():
+        
+    print('1. Cadastrar Restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar Reustaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
     print('Encerrando programa')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # print(f'Você enscolheu a opção {opcao_escolhida}')
+
+    match opcao_escolhida:
+        case  1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção inválida!')
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+    
+if __name__ == '__main__':
+    main()
