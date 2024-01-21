98b04b2661f0ada6f4489de47542beac8525e580
André Luis Quiosi <andre.quiosi@outlook.com>
2024-01-21 12:40:44 -0300

    Criação das funções: cadastro de restaurante e listar restaurante.
    Conjunto de listas;
    Blocos Try Exept;
    Laço de repetição;

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Lazis','sushiTown']
+
 def exibir_nome_do_programa():
     print("""
     ████████████████████████████████████████
@@ -16,26 +18,58 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    print('Encerrando programa')
+    exibir_subtitulo('Encerrando programa')
+
+def opcao_invalida():
+    input('Opção inválida!\nPressione uma tecla para voltar!')
+    main()
+    
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastrar Restaurante')
+
+    nome_do_restaurante = input('Digite o nome do restaurante:')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado!')
+    
+    voltar_ao_menu_principal()
+    
+def listar_restaurantes():
+    exibir_subtitulo('Listar restaurantes')
+    
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+    
+    voltar_ao_menu_principal()
+      
+def voltar_ao_menu_principal():
+        input('\nPressione uma tecla para voltar!')
+        main()
 
 def escolher_opcoes():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # print(f'Você enscolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    match opcao_escolhida:
-        case  1:
-            print('Cadastrar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção inválida!')
+        match opcao_escolhida:
+            case  1:
+                cadastrar_novo_restaurante()
+            case 2:
+                listar_restaurantes()
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
     
+def exibir_subtitulo(texto): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(texto)
+    print()
+
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcoes()
